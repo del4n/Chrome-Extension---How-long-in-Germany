2e4c599abb20591a24065559d63e4f49be57deed
Edin Delanovic <edin.delanovic@smava.de>
2016-03-18 10:30:06 +0100

    Added "Include current day" option

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,24 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-		
-	chrome.storage.sync.get({
-		header: "I am here ",
-		date: "2016-01-01",
-		footer: "days "
-	}, function (obj) {
-		document.getElementById('header').innerHTML  = obj.header;
-		var startDate = moment(obj.date);
-		document.getElementById('footer').innerHTML  = obj.footer;
-		
-		var checkPageButton = document.getElementById('checkPage');
-		var daysDiff = daysBetween(startDate).toString(); 
-		document.getElementById('dayVal').innerHTML = daysDiff;
-		chrome.browserAction.setBadgeText({
-			text: daysDiff
-		});
-	});
-	
+
+    chrome.storage.sync.get({
+        header: "I am here ",
+        date: "2016-01-01",
+        footer: "days ",
+        currentDay: false
+    }, function (obj) {
+        document.getElementById('header').innerHTML  = obj.header;
+        var startDate = moment(obj.date);
+        document.getElementById('footer').innerHTML  = obj.footer;
+
+        var daysDiff = obj.currentDay ? (daysBetween(startDate) + 1) : daysBetween(startDate);
+        console.log(obj.currentDay);
+        console.log(daysBetween(startDate));
+        document.getElementById('dayVal').innerHTML = daysDiff;
+        chrome.browserAction.setBadgeText({
+            text: daysDiff
+        });
+    });
+
     function daysBetween(startDate) {
-        return moment().diff(startDate, 'days');
+            return moment().diff(startDate, 'days');
     }
 });
 
